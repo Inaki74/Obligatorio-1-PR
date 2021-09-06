b62df719a42c4a8f8caf0967684400bca765938b
SleepyWolfy <inaki.2012@hotmail.com>
2021-09-06 16:54:30 -0300

    Created first projects.

---
+++ ServerApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mati!");
+        }
+    }
+}
