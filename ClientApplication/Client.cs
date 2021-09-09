b89bd28349878774b8e73fb9d6342b6e80db2da9
Matias González <matias.gonzalez.2407@gmail.com>
2021-09-09 17:30:09 -0300

    Added Client Handler

--- ClientApplication/Client.cs
+++ ClientApplication/Client.cs
@@ -9,32 +9,14 @@ namespace ClientApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Client starting...");
-           
-            var clientIpEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0); // Puerto 0 -> usa el primer puerto disponible
-            var serverIpEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6000);
-            
-            var tcpClient = new TcpClient(clientIpEndPoint);
-            
-            Console.WriteLine("Trying to connect to server");
-            tcpClient.Connect(serverIpEndPoint);
 
-            Console.WriteLine("Found server!");
+            ClientHandler clientHandler = new ClientHandler();
             
-            SendMessage(tcpClient);
+            clientHandler.StartClient();
+            
+            clientHandler.Loop();
+
         }
-
-        static void SendMessage(TcpClient tcpClient)
-        {
-            using (var networkStream = tcpClient.GetStream())
-            {
-                var word = Console.ReadLine();
-                byte[] data = Encoding.UTF8.GetBytes(word);
-
-                networkStream.Write(data, 0, data.Length);
-            }
-
-            tcpClient.Close();
-        }
+        
     }
 }
