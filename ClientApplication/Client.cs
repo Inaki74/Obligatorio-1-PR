2399d0184af13c75f0d41553e66df003904746d1
SleepyWolfy <inaki.2012@hotmail.com>
2021-09-09 14:41:16 -0300

    Trying to establish first connections between one client and the server.

---
+++ ClientApplication/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApplication
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client starting...");
+           
+            var clientIpEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.77"), 0); // Puerto 0 -> usa el primer puerto disponible
+            var serverIpEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.77"), 6000);
+            
+            var tcpClient = new TcpClient(clientIpEndPoint);
+            
+            Console.WriteLine("Trying to connect to server");
+            tcpClient.Connect(serverIpEndPoint);
+
+            Console.WriteLine("Found server!");
+
+            tcpClient.Close();
+
+            //SendMessage(tcpClient);
+        }
+
+        static void SendMessage(TcpClient tcpClient)
+        {
+            using (var networkStream = tcpClient.GetStream())
+            {
+                var word = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(word);
+
+                networkStream.Write(data, 0, data.Length);
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
---
+++ ServerApplication/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Common;
+
+namespace ServerApplication
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server starting...");
+
+            var serverIpEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.175"), 6000);
+           
+            var tcpServer = new TcpClient(serverIpEndPoint);
+
+            tcpServer.Start(100);
+
+            Console.WriteLine("Listening for clients...");
+
+            while(true)
+            {
+                var clientFound = tcpServer.AcceptTcpClient();
+                Console.WriteLine("Client found!");
+
+                var clientThread = new Thread(() => HandleClient(clientFound));
+                clientThread.Start();
+            }
+        }
+
+        private void HandleClient(TcpClient acceptedTcpClient)
+        {
+            try
+            {
+                NetworkStream stream = acceptedTcpClient.GetStream();
+
+                stream.Read();
+            }
+            catch(SocketException e)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+            
+
+            Console.WriteLine("Goodbye client!");
+        }
+    }
+}
