7e76f9b63561e2ec4c3eb820cfc4a3c8049c9bb3
SleepyWolfy <inaki.2012@hotmail.com>
2021-09-24 10:49:40 -0300

    Added publish review menu logic.

--- ConsoleMenus/Client/ConsolePublishReviewMenu.cs
+++ ConsoleMenus/Client/ConsolePublishReviewMenu.cs
@@ -1,5 +1,8 @@
 using System;
+using ClientApplicationInterfaces;
+using Common.Protocol.NTOs;
 using ConsoleMenusInterfaces;
+using Domain.BusinessObjects;
 
 namespace ConsoleMenus.Client
 {
@@ -11,12 +14,79 @@ namespace ConsoleMenus.Client
 
         public bool Action(string answer)
         {
+            Review review = GetReviewInput();
+            ReviewNetworkTransferObject reviewNTO = new ReviewNetworkTransferObject();
+            reviewNTO.Load(review);
+
+            string response = IClientHandler.Instance.PublishReview(reviewNTO);
+
+            _nextMenu = new ConsoleGameMenu(false);
+            Console.WriteLine(response);
+
             return false;
         }
 
         public void PrintMenu()
         {
+            Console.WriteLine("Write a review of the game. You must provide a score (1 to 5).\n");
+        }
+
+        private Review GetReviewInput()
+        {
+            Console.WriteLine("Write a review about the game. Note: You can't use enter.");
+            string description = Console.ReadLine();
+
             Console.WriteLine("");
+            Console.WriteLine("Score:");
+            string score = Console.ReadLine();
+
+            while(!VerifyScore(ref score))
+            {
+                Console.WriteLine("\nSCORE input incorrect, reenter:");
+                score = Console.ReadLine();
+            }
+
+            if(score == "0")
+            {
+                Console.WriteLine("\nNo score given. A score is required. Returning to game menu.");
+                return null;
+            }
+
+            Review ret = new Review();
+            ret.Description = description;
+            ret.Score = int.Parse(score);
+
+            return ret;
+        }
+
+        private bool VerifyScore(ref string score)
+        {
+            bool isCorrect = false;
+
+            if(string.IsNullOrEmpty(score))
+            {
+                score = "0";
+                return true;
+            }
+
+            try
+            {
+                int parsedScore = int.Parse(score);
+                int MAXSCORE = 5; // TODO: Make it a constant somewhere.
+                for(int i = 1; i <= MAXSCORE; i++)
+                {
+                    if(parsedScore == i)
+                    {
+                        isCorrect = true;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                isCorrect = false;
+            }
+
+            return isCorrect;
         }
     }
 }
