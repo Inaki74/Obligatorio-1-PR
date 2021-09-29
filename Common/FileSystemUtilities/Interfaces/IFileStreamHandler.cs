c0e19819353238228a29bb316434b615a96fae6d
Matias González <matias.gonzalez.2407@gmail.com>
2021-09-28 21:31:44 -0300

    Fixed SendCover bug

--- Common/FileSystemUtilities/Interfaces/IFileStreamHandler.cs
+++ Common/FileSystemUtilities/Interfaces/IFileStreamHandler.cs
@@ -4,7 +4,7 @@ namespace Common.FileSystemUtilities.Interfaces
 {
     public interface IFileStreamHandler
     {
-        void Write(byte[] data, string path);
+        void Write(byte[] data, string path, bool firstPart);
         
         byte[] Read(string path, long offset, int length);
     }
